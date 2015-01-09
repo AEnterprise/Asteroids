8c7bb69034e42bc325b6716fd01efc8ef873be13
AEnterprise <AEnterprise2@gmail.com>
2015-01-09 14:34:44 +0100

    abstraction away of point generation

--- Asteroids/Paint.cpp
+++ Asteroids/Paint.cpp
@@ -6,13 +6,13 @@ void paint(HDC hdc) {
 	pn = CreatePen(PS_SOLID, 2, RGB(0, 0, 255));
 	bn = CreateSolidBrush(RGB(0, 0, 255));
 	po = (HPEN)SelectObject(hdc, pn);
+	int numbers[] = { 100, 100, 120, 100, 100, 120 };
 	POINT points[3];
-	points[0].x = 100;
-	points[0].y = 100;
-	points[1].x = 500;
-	points[1].y = 100;
-	points[2].x = 300;
-	points[2].y = 200;
+	generatePoints(points, numbers);
 	Polygon(hdc, points, 3);
+	
+}
+
+void generatePoints(POINT array[], int coordinates[], int amount) {
 	
 }
--- Asteroids/Paint.h
+++ Asteroids/Paint.h
@@ -1,3 +1,4 @@
 #include "stdafx.h"
 
 void paint(HDC hdc);
+void generatePoints(POINT array[], int coordinates[], int amount);
